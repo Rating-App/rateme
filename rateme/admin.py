021f85a3121af13897cdab9e2b0cbb1615687285
neuro <asuka2ndchild@gmail.com>
2019-09-16 03:10:44 +0300

    register Recommendations at admin.py

--- rateme/admin.py
+++ rateme/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Rating, RatingCard, Tag, Movie
+from .models import Rating, RatingCard, Tag, Movie, Recommendation
 
 admin.site.register(Rating)
 admin.site.register(RatingCard)
 admin.site.register(Tag)
 admin.site.register(Movie)
+admin.site.register(Recommendation)
